2914e4d28103bddca1dd82667ae779585a41e579
Gordon Fontenot <gordon@thoughtbot.com>
2014-01-07 10:58:53 -0500

    Initial Commit

---
+++ NSStringEncryption/TBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TBAppDelegate.h
+//  NSStringEncryption
+//
+//  Created by Gordon Fontenot on 1/7/14.
+//  Copyright (c) 2014 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
